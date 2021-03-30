477a753a54ea033b7fb7c930b931b233c18291e3
harshithreddy22 <81611574+harshithreddy22@users.noreply.github.com>
2021-03-30 12:55:53 +0530

    Update BinToDeci.java

--- 01_loop/BinToDeci.java
+++ 01_loop/BinToDeci.java
@@ -1,13 +1,4 @@
-/*
-This Program was downloaded from this repository:
-https://github.com/ApkaGuruji/ISC_ASSIGNMENT_PROGRAMS
-=========== Apka Guruji ==============
-for more free coding resources for ICSE, ISC, CBSE Students
-Visit us:
-Website: ApkaGuruji.com
-Youtube: https://www.youtube.com/ApkaGuruji
-GitHub: https://github.com/ApkaGuruji
-*/
+
 
 //WAP in java to accept a binary number and convert to decimal equivalent
 
